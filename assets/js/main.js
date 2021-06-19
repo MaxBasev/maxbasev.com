f1b479a735e188dba5536d2a6a5a053fc7f6c6c6
MaxBasev <maxbasev@gmail.com>
2021-06-19 17:21:41 +0700

    Del CounterUp.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,11 +108,11 @@
     return false;
   });
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
+  // // jQuery counterUp
+  // $('[data-toggle="counter-up"]').counterUp({
+  //   delay: 10,
+  //   time: 1000
+  // });
 
   // Skills section
   $('.skills-content').waypoint(function () {
